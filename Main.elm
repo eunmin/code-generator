05b714f4c92f549bd738ec21102c0b91755a18de
Eunmin Kim <telepopsound@gmail.com>
2017-08-30 22:13:28 +0900

    :)

--- Main.elm
+++ Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Bootstrap.CDN as CDN
+import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
 import Bootstrap.Form.Textarea as Textarea
 import Bootstrap.Grid as Grid
@@ -13,6 +14,8 @@ import MD5 exposing (..)
 type alias Model =
     { value : String
     , code : String
+    , values : List String
+    , codes : List String
     }
 
 
@@ -20,11 +23,14 @@ initModel : Model
 initModel =
     { value = ""
     , code = ""
+    , values = []
+    , codes = []
     }
 
 
 type Msg
     = ChangeValue String
+    | ChangeMultiValue String
 
 
 encodeValue : String -> String
@@ -44,29 +50,75 @@ update msg model =
                 , code = encodeValue value
             }
 
+        ChangeMultiValue valuesStr ->
+            let
+                values =
+                    String.split "\n" valuesStr
+            in
+            { model
+                | values = values
+                , codes =
+                    List.map
+                        (\value ->
+                            if value == "" then
+                                ""
+                            else
+                                value ++ "," ++ encodeValue value
+                        )
+                        values
+            }
+
 
 view : Model -> Html Msg
 view model =
     Grid.container []
         [ CDN.stylesheet
-        , h1 [] [ text "md5" ]
+        , h1 [] [ text "code!" ]
         , Grid.row []
             [ Grid.col []
-                [ h5 [] [ text "value" ] ]
-            , Grid.col []
-                [ h5 [] [ text "code" ] ]
-            ]
-        , Grid.row []
-            [ Grid.col []
-                [ Input.text
-                    [ Input.attrs
-                        [ value model.value
-                        , onInput ChangeValue
+                [ Form.form []
+                    [ Form.group []
+                        [ Form.label [] [ text "value" ]
+                        , Input.text
+                            [ Input.attrs
+                                [ value model.value
+                                , onInput ChangeValue
+                                ]
+                            ]
+                        , Form.group []
+                            [ Form.label [] [ text "" ]
+                            , Textarea.textarea
+                                [ Textarea.attrs
+                                    [ style [ ( "height", "500px" ) ]
+                                    , onInput ChangeMultiValue
+                                    , value (String.join "\n" model.values)
+                                    ]
+                                ]
+                            ]
                         ]
                     ]
                 ]
             , Grid.col []
-                [ Input.text [ Input.attrs [ value model.code ] ] ]
+                [ Form.form []
+                    [ Form.group []
+                        [ Form.label [] [ text "code" ]
+                        , Input.text
+                            [ Input.attrs
+                                [ value model.code ]
+                            ]
+                        , Form.group []
+                            [ Form.label [] [ text "" ]
+                            , Textarea.textarea
+                                [ Textarea.attrs
+                                    [ style
+                                        [ ( "height", "500px" ) ]
+                                    , value (String.join "\n" model.codes)
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
             ]
         ]
 
